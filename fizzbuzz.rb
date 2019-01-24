4a6b472ef686dfbcce5ffb2bf2cad31b50ae7e38
mggbhn <maggiebswim@gmail.com>
2019-01-24 14:28:54 +0000

    Automatically backed up by Learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,28 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz" 
+  end
+end
+
+puts fizzbuzz
+
+
+def fizzbuzz(int=gets.to_i)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz" 
+  else
+    puts int
+  end
+end
+
+puts fizzbuzz
+
+
